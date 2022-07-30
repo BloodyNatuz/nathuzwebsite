dbd00b9b6e86e03a1a58e26e3b71d3bd1b59ef03
Nathuz <nathuznoepupin@gmail.com>
2022-07-31 01:14:01 +0200

    FIX fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,9 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +12,15 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $mainuser = NEW User();
+        $mainuser->setName('Nathuz');
+        $mainuser->setRoles(['ROLE_ADMIN']);
+        $mainuser->setPassword($this->encoder->hashPassword($mainuser, 'Noepupdu50!!'));
+        $mainuser->setEmail('nathuznoepupin@gmail.com');
+        $mainuser->setAdress('96 rue Carnot, 50120');
+        $mainuser->setTel('+33 6 72 68 87 53');
+        $manager->persist($mainuser);
 
         $manager->flush();
     }
