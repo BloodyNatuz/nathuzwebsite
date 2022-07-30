c24c581386a284a8043fccb426aa213af417ccdb
Nathuz <nathuznoepupin@gmail.com>
2022-07-31 00:38:53 +0200

    ADD WorksController

---
+++ src/Controller/WorksController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WorksController extends AbstractController
+{
+    /**
+     * @Route("/works", name="app_works")
+     */
+    public function index(): Response
+    {
+        return $this->render('works/index.html.twig', [
+            'controller_name' => 'WorksController',
+        ]);
+    }
+}
