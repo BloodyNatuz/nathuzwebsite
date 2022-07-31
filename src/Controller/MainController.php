9373fbbc44a6649e233b511b90e8805419edacff
Nathuz <nathuznoepupin@gmail.com>
2022-07-31 03:10:50 +0200

    FIX services IN index

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Service;
+use App\Repository\ServiceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="app_main")
      */
-    public function index(): Response
+    public function index(ServiceRepository $serviceRepo): Response
     {
         return $this->render('main/index.html.twig', [
             'page_title' => 'Nathuz Website - Accueil',
+            'services' => $serviceRepo->findAll(),
         ]);
     }
 }
