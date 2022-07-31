29af3b7a914581d7437d646238b19ecd3d659549
Nathuz <nathuznoepupin@gmail.com>
2022-07-31 05:46:15 +0200

    ADD about

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ExpRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AboutController extends AbstractController
 {
     /**
      * @Route("/about", name="app_about")
      */
-    public function index(): Response
+    public function index(ExpRepository $expRepo): Response
     {
         return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutController',
+            'page_title' => 'Nathuz - A propos',
+            'exps' => $expRepo->findAll(),
         ]);
     }
 }
