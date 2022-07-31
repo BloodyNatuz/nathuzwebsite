707e588511a96f8b229710e7745e04f845081413
Nathuz <nathuznoepupin@gmail.com>
2022-07-31 05:54:57 +0200

    FIX contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepo): Response
     {
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'page_title' => 'Nathuz - Contact',
+            'users' => $userRepo->findAll(),
         ]);
     }
 }
